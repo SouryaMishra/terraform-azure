ed2604a392798196c6bdd860abe0a439ec3d6279
Sourya Mishra <sourya.mishra111@gmail.com>
2022-11-08 23:13:16 +0530

    create network interface

--- main.tf
+++ main.tf
@@ -78,6 +78,23 @@ resource "azurerm_public_ip" "my-ip" {
   }
 }
 
+resource "azurerm_network_interface" "my-nic" {
+  name                = "myNIC"
+  resource_group_name = azurerm_resource_group.my-rg.name
+  location            = azurerm_resource_group.my-rg.location
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.my-subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.my-ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
+
 moved {
   from = azurerm_resource_group.sm-rg
   to   = azurerm_resource_group.my-rg
