bcf4ab89f357bb8ef98162ec22651e998d6253d4
Sourya Mishra <sourya.mishra111@gmail.com>
2022-11-08 21:51:06 +0530

    create virtual network

--- main.tf
+++ main.tf
@@ -22,7 +22,18 @@ resource "azurerm_resource_group" "my-rg" {
   }
 }
 
+resource "azurerm_virtual_network" "my-vn" {
+  name                = "myNetwork"
+  resource_group_name = azurerm_resource_group.my-rg.name
+  location            = azurerm_resource_group.my-rg.location
+  address_space       = ["10.123.0.0/16"]
+
+  tags = {
+    environment = "dev"
+  }
+}
+
 moved {
   from = azurerm_resource_group.sm-rg
-  to = azurerm_resource_group.my-rg
+  to   = azurerm_resource_group.my-rg
 }
