5b7ff3f3e82206d72d1e35f73163fcbfe0f8859e
Sourya Mishra <sourya.mishra111@gmail.com>
2022-11-08 23:46:38 +0530

    create ssh keys and linux virtual machine

--- main.tf
+++ main.tf
@@ -95,6 +95,32 @@ resource "azurerm_network_interface" "my-nic" {
   }
 }
 
+resource "azurerm_linux_virtual_machine" "my-linux-vm" {
+  name                  = "myLinuxVM"
+  resource_group_name   = azurerm_resource_group.my-rg.name
+  location              = azurerm_resource_group.my-rg.location
+  size                  = "Standard_B1s"
+  admin_username        = "adminuser"
+  network_interface_ids = [azurerm_network_interface.my-nic.id]
+
+  admin_ssh_key {
+    username   = "adminuser"
+    public_key = file("~/.ssh/azure_key.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
 moved {
   from = azurerm_resource_group.sm-rg
   to   = azurerm_resource_group.my-rg
